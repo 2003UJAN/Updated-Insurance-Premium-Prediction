4e4edb610a1e037ddbf9e4373a3e4d824d79380c
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-12-28 12:17:28 +0530

    Update shap_analysis.py

--- src/shap_analysis.py
+++ src/shap_analysis.py
@@ -1,1 +1,14 @@
+import shap
+import joblib
+import pandas as pd
+
+model = joblib.load("/app/model.pkl")
+df = pd.read_csv("/data/insurance_premium_dataset.csv")
+
+X = df.drop("annual_premium", axis=1)
+
+explainer = shap.Explainer(model)
+shap_values = explainer(X)
+
+shap.summary_plot(shap_values, X)
 
