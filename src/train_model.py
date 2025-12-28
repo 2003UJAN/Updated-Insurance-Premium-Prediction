940cc3bf2f0cc4c2531b0ecbffe7c1a1eeb78bc7
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-12-28 12:18:16 +0530

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,1 +1,40 @@
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from xgboost import XGBRegressor
+from sklearn.metrics import r2_score
+
+df = pd.read_csv("/data/synthetic_insurance_premium_dataset.csv")
+
+cat_cols = ["city", "locality_category", "policy_type", "occupation_type"]
+encoders = {}
+
+for col in cat_cols:
+    le = LabelEncoder()
+    df[col] = le.fit_transform(df[col])
+    encoders[col] = le
+
+X = df.drop("annual_premium", axis=1)
+y = df["annual_premium"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = XGBRegressor(
+    n_estimators=300,
+    max_depth=6,
+    learning_rate=0.08,
+    subsample=0.9,
+    colsample_bytree=0.9
+)
+
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+print("R2 Score:", r2_score(y_test, pred))
+
+joblib.dump(model, "../app/model.pkl")
+joblib.dump(encoders, "../app/encoders.pkl")
 
