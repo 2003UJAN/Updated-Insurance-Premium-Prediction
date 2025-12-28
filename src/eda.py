ce45c09ecb33f193ffd464d9b013303e8baa07f7
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-12-28 12:16:53 +0530

    Update eda.py

--- src/eda.py
+++ src/eda.py
@@ -1,1 +1,25 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("/data/insurance_premium_dataset.csv")
+
+print(df.info())
+print(df.describe())
+
+# Distribution
+sns.histplot(df["annual_premium"], kde=True)
+plt.title("Premium Distribution")
+plt.show()
+
+# City-wise premium
+plt.figure(figsize=(10,5))
+sns.boxplot(x="city", y="annual_premium", data=df)
+plt.xticks(rotation=30)
+plt.show()
+
+# Correlation
+plt.figure(figsize=(12,8))
+sns.heatmap(df.select_dtypes("number").corr(), annot=True, cmap="coolwarm")
+plt.show()
 
